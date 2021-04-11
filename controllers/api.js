1b0d03eed544fda13d342bb44b5a45740da11a28
didihottest <didihottest@gmail.com>
2021-04-11 21:18:51 +0700

    add some comments

--- controllers/api.js
+++ controllers/api.js
@@ -61,9 +61,10 @@ exports.getUser = (async (req, res, next) => {
 // add new user api end point
 exports.newUser = (multer().none(), async (req, res, next) => {
   const { username, password, firstName, lastName, age, win, lose } = req.body;
-  const user_biodataID = new mongoose.Types.ObjectId()
-  const user_historyID = new mongoose.Types.ObjectId()
+  const user_biodataID = new mongoose.Types.ObjectId() // generate unique id for user_biodataID
+  const user_historyID = new mongoose.Types.ObjectId() // user_historyID
   try {
+    // insert new data to user_games collection
     const newUser = new User_Game({
       _id: new mongoose.Types.ObjectId(),
       username: username,
@@ -71,7 +72,7 @@ exports.newUser = (multer().none(), async (req, res, next) => {
       userGameBiodata: user_biodataID,
       userGameHistory: user_historyID
     })
-
+    // insert new data to user_game_biodatas collection
     const user_biodata = new User_Game_Biodata({
       _id: user_biodataID,
       user_id: newUser._id,
@@ -79,27 +80,26 @@ exports.newUser = (multer().none(), async (req, res, next) => {
       lastName: lastName,
       age: age
     })
-
-
+    // insert new data to user_game_histories collection
     const user_history = new User_Game_History({
       _id: user_historyID,
       user_id: newUser._id,
       win: win,
       lose: lose
     })
-
+    // save new data to collections
     user_biodata.save((err) => {
       if (err) {
         console.error(err)
       }
     })
-
+    // save new data to collections
     user_history.save((err) => {
       if (err) {
         console.error(err)
       }
     })
-
+    // save new data to collections
     newUser.save((err) => {
       // if username is not unique tell dashboard that there is duplicated data
       if (err) {
@@ -113,8 +113,6 @@ exports.newUser = (multer().none(), async (req, res, next) => {
         }
       }
     })
-
-    console.log("user value" + newUser)
 
   } catch (error) {
     res.send({
@@ -166,14 +164,15 @@ exports.editUser = (multer().none(), async (req, res, next) => {
 exports.deleteUser = (async (req, res) => {
   const id = req.params.id;
   try {
-    await User_Game.findOneAndDelete({ _id: id }, async (err, res) => {
+    await User_Game.findOneAndDelete({ _id: id }, async (err, res) => { // delete user_games collections
       try {
-        await User_Game_Biodata.deleteOne({ _id: res.userGameBiodata })
-        await User_Game_History.deleteOne({ _id: res.userGameHistory })
+        await User_Game_Biodata.deleteOne({ _id: res.userGameBiodata })// delete user_game_bioadatas collections
+        await User_Game_History.deleteOne({ _id: res.userGameHistory })// delete user_game_histories collections
       } catch (error) {
         console.error(error.message)
       }
     })
+    // redirect if success
     res.redirect("/dashboard?status=successdelete")
   } catch (error) {
     res.send({
