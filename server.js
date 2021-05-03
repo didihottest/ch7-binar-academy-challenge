a1a2c701acc2ceb30142dcd1a0b8cfdae60c2936
didihottest <didihottest@gmail.com>
2021-05-03 17:10:17 +0700

    add comment

--- server.js
+++ server.js
@@ -37,31 +37,32 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
 }))
+// use flash for error handling
 app.use(flash())
 app.use(routers)
 
 
-// // end point if server has an internal error
-// app.use((error, req, res, next) => {
-//     res.status(500).render("error", {
-//         headTitle: "Internal Server Error",
-//         title: "Internal Server Error",
-//         subtitle: "Go to Main Page",
-//         location: "/"
-//     })
-//     next()
-// });
+// end point if server has an internal error
+app.use((error, req, res, next) => {
+    res.status(500).render("error", {
+        headTitle: "Internal Server Error",
+        title: "Internal Server Error",
+        subtitle: "Go to Main Page",
+        location: "/"
+    })
+    next()
+});
 
-// // end point if server has dont have the requested end point
-// app.use((req, res, next) => {
-//     res.status(404).render("error", {
-//         headTitle: "Not Found!",
-//         title: "404 Not Found",
-//         subtitle: "Go To Main Page",
-//         location: "/"
-//     });
-//     next()
-// });
+// end point if server has dont have the requested end point
+app.use((req, res, next) => {
+    res.status(404).render("error", {
+        headTitle: "Not Found!",
+        title: "404 Not Found",
+        subtitle: "Go To Main Page",
+        location: "/"
+    });
+    next()
+});
 
 // set server to listen to localhost:3000 
 const PORT = process.env.PORT || 3000;
