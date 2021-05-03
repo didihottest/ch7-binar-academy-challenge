b7a63d264800697cb8a39b976939d5d102755b23
didihottest <didihottest@gmail.com>
2021-05-03 17:05:11 +0700

    add comment

--- middleware/authAdminMiddleware.js
+++ middleware/authAdminMiddleware.js
@@ -1,51 +1,59 @@
 const jwt = require('jsonwebtoken')
 const User_Admin = require('../model/user_admin')
 
-const requireAuth = (req, res, next)=>{
-
-
-
-
-
+// check authentification from cookies
+const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt
 
   // check json web token exist & verified
   if (token) {
     jwt.verify(token, 'secret', (err, decodedToken) => {
-      if(err){
+      if (err) {
+        // redirect to login if error
         res.redirect('/login-dashboard')
       } else {
-        if(decodedToken.role != 'admin') {
+        // redirect to login if error and give error message using flash
+
+        if (decodedToken.role != 'admin') {
           req.flash('error', "you need to be admin to access this page")
           res.redirect('/login-dashboard')
         }
+        // next to another middleware if success
         next();
       }
     })
   } else {
+    // redirect to login if error
+
     res.redirect('/login-dashboard')
   }
 }
 
-// check current user
-const checkUser = (req, res, next)=>{
+// check who is the current user
+const checkUser = (req, res, next) => {
   const token = req.cookies.jwt
 
-  if (token){
+  if (token) {
+    // verify token
     jwt.verify(token, 'secret', async (err, decodedToken) => {
-      if(err){
+      if (err) {
+        // set user to null if token error
         res.locals.user = null
         next();
       } else {
+        // find user from database and assign it res.locals.user
         let user = await User_Admin.findById(decodedToken.id)
         res.locals.user = user.email
+        // next to another middleware
         next();
       }
     })
   } else {
+    // set to null if error
     res.locals.user = null
+    // next to another middleware
     next();
   }
 }
-//
-module.exports= {requireAuth, checkUser}
+// export modules
+module.exports = { requireAuth, checkUser }
